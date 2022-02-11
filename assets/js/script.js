9a3d19cf56e73d233925d60644878091e7e122c5
Robertus06 <54945514+Robertus06@users.noreply.github.com>
2022-02-11 21:12:32 +0100

    Fixed error

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,13 @@
 var btnAbrirPopup = document.getElementById('btn-abrir-popup'),
 	overlay = document.getElementById('overlay'),
+	closePopup = document.getElementById('closePopup'),
 	popup = document.getElementById('popup'),
 	btnCerrarPopup = document.getElementById('btn-cerrar-popup');
+	cerrarPopup = document.getElementById('cerrar-popup');
 
 btnAbrirPopup.addEventListener('click', function(){
 	overlay.classList.add('active');
+	closePopup.classList.add('active');
 	popup.classList.add('active');
 	var x = window.scrollX;
 	var y = window.scrollY;
@@ -14,6 +17,15 @@ btnAbrirPopup.addEventListener('click', function(){
 btnCerrarPopup.addEventListener('click', function(e){
 	e.preventDefault();
 	overlay.classList.remove('active');
+	closePopup.classList.remove('active');
+	popup.classList.remove('active');
+	window.onscroll = null;
+});
+
+cerrarPopup.addEventListener('click', function(e){
+	e.preventDefault();
+	overlay.classList.remove('active');
+	closePopup.classList.remove('active');
 	popup.classList.remove('active');
 	window.onscroll = null;
 });
